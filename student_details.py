e6cac5547f9937221bef2a8a9394d099fb302ae1
hari11894 <hari11894@gmail.com>
2017-07-11 12:05:10 +0530

    first edit

---
+++ student_details.py
@@ -0,0 +1,12 @@
+"""
+First, I created an empty dictionary.
+Secondly, I added entries(key/value) to the empty dictionary.
+
+"""
+
+student_info={}
+student_info["name"]="Oscar"
+student_info["age"]=23
+student_info["dept"]="computer science"
+student_info["level"]=300
+print("Student details:",student_info) 
